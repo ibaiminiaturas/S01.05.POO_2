7aaa802a6d134207af0919ff82015b8ba0a66032
Ibai <ibaiminiaturas@gmail.com>
2025-05-19 11:30:42 +0200

    changed method name for construct because apparently I don't know how to spell construct...

--- Exercise1/index.php
+++ Exercise1/index.php
@@ -1,4 +1,3 @@
-
 <!--Sprint 1. Chapter 5. Exercise 1
 
 Necessitem crear un tipus de dades que representi un animal. Els animals tenen un nom, ara bé, no és el mateix el so de la “parla” d’un gos, que el d’un gat. 
@@ -7,24 +6,33 @@ Bàsicament, volem un mètode makeSound() que mostri un missatge diferent si es 
 
 <?php
 
-abstract class Animal{
+abstract class Animal
+{
     protected $name;
-    
-    public function __consrtruct($name){
+
+    public function __construct($name)
+    {
         $this->name = $name;
     }
 
-    abstract public function makeSound(): string;   
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    abstract public function makeSound(): string;
 }
 
-class Dog extends Animal{
+class Dog extends Animal
+{
     public function makeSound(): string
     {
         return "Bup, bup!";
     }
 }
 
-class Cat extends Animal{
+class Cat extends Animal
+{
     public function makeSound(): string
     {
         return "Meuuuu!";
@@ -35,7 +43,7 @@ class Cat extends Animal{
 $dog1 = new Dog("Perrete");
 $cat1 = new Cat("Gatete");
 
-echo $dog1->makeSound() . PHP_EOL;
-echo $cat1->makeSound() . PHP_EOL;
+echo "El " . $dog1->getName() . " hace: " . $dog1->makeSound() . PHP_EOL;
+echo "El " . $cat1->getName() . " hace: " . $cat1->makeSound() . PHP_EOL;
 
 ?>
